97a6b301cc3c8c8575f37425508b17637eb584c2
Seth-Miller <sethmiller.sm@gmail.com>
2024-08-07 12:14:13 -0500

    exascale lab script changes

--- exadata-features/files/lab_exascale_clone_07.sql
+++ exadata-features/files/lab_exascale_clone_07.sql
@@ -4,4 +4,4 @@ SELECT ROUND(hc_space_used/1073741824) space_used_gb
 FROM v$exa_vault
 WHERE LOWER(vault_name) = (SELECT LOWER(name) || 'vault' FROM v$database);
 SELECT 'FULL CLONE' type, &hcsu_b - &hcsu_a space_used_raw, ROUND((&hcsu_b - &hcsu_a) / 2) space_used_actual UNION 
-SELECT 'THIN CLONE' type, &hcsu_c - &hcsu_b space_used_raw, ROUND((&hcsu_c - &hcsu_b) / 2) space_used_actual FROM DUAL;
+SELECT 'THIN CLONE' type, &hcsu_c - &hcsu_b space_used_raw, ROUND((&hcsu_c - &hcsu_b) / 2, 1) space_used_actual FROM DUAL;
